f8793c902f56ccf667ac8af3a81ed2e75ab0455b
felixjamestin <felixjamestin@gmail.com>
2017-06-26 18:15:43 +0530

    Fix: Changed splash screens

--- src/components/common/AppStyles.js
+++ src/components/common/AppStyles.js
@@ -1,7 +1,6 @@
 const AppStyles = {
   colors: {
     appBackground: "#330C2F",
-    // redBackground: "#290000",
     redText: "#81000F",
     redSecondaryDark: "#8A061A",
     redSecondary: "#A3071E",
@@ -10,14 +9,31 @@ const AppStyles = {
     green: "green"
   },
   rowColors: {
-    red: "#E50F2E",
+    a: "#774CC5",
+    b: "#32936F",
+    c: "#4BA3D3",
+    d: "#FFBC42",
+    e: "#E83F6F"
+  },
+  rowColors1: {
     violet: "#774CC5",
-    magenta: "#E51F58",
-    turquoise: "#4BA3D3"
-    // blue: "#3066BE",
-    // pink: "#9D44B5"
-    // yellow: "#F9DC5C"
-    // green: "#44AF59"
+    turquoise: "#4BA3D3",
+    blue: "#2541B2",
+    green: "#02C39A"
+  },
+  rowColors2: {
+    a: "#20BF55",
+    b: "#0B4F6C",
+    c: "#01BAEF",
+    d: "#FBFBFF",
+    e: "#757575"
+  },
+  rowColors3: {
+    a: "#774CC5",
+    b: "#006BA6",
+    c: "#0496FF",
+    d: "#FFBC42",
+    e: "#D81159"
   }
 };
 
